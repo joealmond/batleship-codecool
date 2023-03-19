d4c22e1a90f4cec712e3e210aa05e433887b236d
joealmond <jozsef.mandula@gmail.com>
2023-03-19 13:08:21 +0100

    correcting ship data in data.js

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 const data = {
   // I modified data to so it can be parsed with JSON.parse()
-  game1: '{"size":4,"s":{"s0":"a0","s1":"c3"}}',
-  game2: '{"size":4,"s":{"s0":"b1","s1":"d3"}}',
-  game3: '{"size":4,"s":{"s0":"a2","s1":"c0"}}',
-  game4: '{"size":5,"s":{"s0":"a2","s1":"c3","s2":"e0"}}',
-  game5: '{"size":6,"s":{"s0":"b0","s1":"e2","s2":"d3d4"}}',
-  game6: '{"size":6,"s":{"s0":"a0a1","s1":"c1d1e1","s2":"c4","s3":"e3"}}',
+  game1: '{"size":4,"s":{"s1":"a1","s2":"c3"}}',
+  game2: '{"size":4,"s":{"s1":"b2","s2":"d4"}}',
+  game3: '{"size":4,"s":{"s1":"a3","s2":"c1"}}',
+  game4: '{"size":5,"s":{"s1":"a3","s2":"c4","s3":"e1"}}',
+  game5: '{"size":6,"s":{"s1":"b1","s2":"e3","s3":"d4d5"}}',
+  game6: '{"size":6,"s":{"s1":"a1a2","s2":"c2d2e2","s3":"c5","s4":"e4"}}',
 };
